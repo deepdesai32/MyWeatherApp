3b6d17a15b1ad925fa127f7035f3bfa1968a7cbd
Deep Desai <deep.hemant.desai@gmail.com>
2020-06-04 23:21:25 -0400

    initial setup

---
+++ MyWeatherApp/MyWeatherApp/Views/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IntroductionViewController.swift
+//  MyWeatherApp
+//
+//  Created by Deep on 2020-06-04.
+//  Copyright © 2020 Deep. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class IntroductionViewController: UIViewController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
