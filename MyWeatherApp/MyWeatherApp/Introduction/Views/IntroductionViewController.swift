f4a6ff4fc86d4d94d022276d93b0468e90540c67
Deep Desai <deep.hemant.desai@gmail.com>
2020-06-05 00:44:40 -0400

    Created Introduction View

--- MyWeatherApp/MyWeatherApp/Introduction/Views/IntroductionViewController.swift
+++ MyWeatherApp/MyWeatherApp/Introduction/Views/IntroductionViewController.swift
@@ -11,31 +11,66 @@ import UIKit
 
 open class IntroductionViewController: UIViewController {
     
-    open var logoImage: UIImage?
+    open var proceedButton: UIButton?
     
     open override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        proceedButton?.addTarget(self, action: #selector(enterPressed(_:)), for: .touchUpInside)
     }
     
+    @objc func enterPressed(_ button: UIButton) {
+        print("Hey")
+    }
+    
+    //setting up introduction view: Logo and Button
     func setupView(){
         view.backgroundColor = UIColor.primary
         
         let logoView: UIImageView = {
             let imageView = UIImageView()
-            imageView.image = UIImage(named: "FlyForcastLogo")
-            imageView.clipsToBounds = true
+            imageView.image = UIImage(named: "FlyForecastLogo")
             imageView.translatesAutoresizingMaskIntoConstraints = false
         
-            return imageView;
+            return imageView
+        }()
+
+        let enterButton: UIButton = {
+            let button = UIButton()
+            button.setTitleColor(.black, for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+            button.setTitle("Enter", for: .normal)
+            button.backgroundColor = .white
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 1
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            return button
         }()
         
         view.addSubview(logoView)
+        view.addSubview(enterButton)
         
         NSLayoutConstraint.activate([
             logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            logoView.widthAnchor.constraint(equalTo: view.widthAnchor)
+            logoView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            logoView.heightAnchor.constraint(equalToConstant: logoView.image?.size.height ?? 500)
         ])
+        
+        NSLayoutConstraint.activate([
+
+            enterButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            enterButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
+            enterButton.heightAnchor.constraint(equalToConstant: 40),
+            enterButton.bottomAnchor.constraint(equalToSystemSpacingBelow: logoView.bottomAnchor, multiplier: 2.0)
+        ])
+        
+        proceedButton = enterButton
+    }
+    
+    //making the view portrait only for all devices
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+            return .portrait
     }
 }
